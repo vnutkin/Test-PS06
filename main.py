e45ed33b9b344a8140dfc87e56fa4a1bb0eaa7f2
vnutkin <vl_utkin@yahoo.com>
2025-01-18 18:38:50 +0300

    последний вариант
    последний вариант, с преобразованием в число цены

--- main.py
+++ main.py
@@ -2,11 +2,12 @@
 import time
 # Импортируем модуль csv
 import csv
+import re
 # Импортируем Selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+#from selenium.webdriver.support.ui import WebDriverWait
+#from selenium.webdriver.support import expected_conditions as EC
 
 
 # Инициализируем браузер
@@ -25,7 +26,7 @@ time.sleep(3)
 lamps = driver.find_elements(By.CLASS_NAME, 'WdR1o')
 
 # Выводим светильники на экран
-#print(lamps)
+print(len(lamps))
 parsed_data = []
 
 # Перебираем коллекцию вакансий
@@ -38,18 +39,34 @@ for lamp in lamps:
       title = lamp.find_element(By.CSS_SELECTOR, 'img.c9h0M').get_attribute('alt')
 # Находим цену
       what = 'p'
-      price = lamp.find_element(By.CSS_SELECTOR, 'span.ui-LD-ZU').text
+      prices = lamp.find_element(By.CSS_SELECTOR, 'div.pY3d2')
+      price = prices.find_element(By.CSS_SELECTOR, "[data-testid='price']").text
 # Находим ссылку с помощью атрибута 'href'
       what = 'l'
-      link = ''#lamp.find_element(By.CSS_SELECTOR, 'a.ui-GPFV8').get_attribute('href')
+      link = lamp.find_element(By.CSS_SELECTOR, 'a.ui-GPFV8').get_attribute('href')
+      match = re.search(r'([\d\s]+(?:\.[\d]+)?)', price)
+
+      if match:
+           # Извлекаем найденное число и преобразуем его в число с плавающей запятой
+        float_price = float(match.group(1).replace(' ', ''))
+      else:
+        float_price = "Цена не найдена"
 # Вставляем блок except на случай ошибки - в случае ошибки программа попытается продолжать
    except Exception as e:
        print("произошла ошибка при парсинге",what,e)
        continue
 
 # Вносим найденную информацию в список
-   parsed_data.append([title, price, link])
+   parsed_data.append([title, price,float_price, link])
 
 # Закрываем подключение браузер
 driver.quit()
-print(parsed_data)
+#print(parsed_data)
+with open("svet.csv", 'w',newline='', encoding='utf-8') as file:
+    # Используем модуль csv и настраиваем запись данных в виде таблицы
+    # Создаём объект
+    writer = csv.writer(file)
+    # Создаём первый ряд
+    writer.writerow(['Наименование товара', 'цена строкой','цена числом', 'ссылка на товар'])
+    # Прописываем использование списка как источника для 'цена',рядов таблицы
+    writer.writerows(parsed_data)
